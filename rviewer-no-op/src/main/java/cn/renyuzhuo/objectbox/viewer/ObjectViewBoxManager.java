dad1f931753f34238f82afa6a35a9c8140bb50c1
RenYuZhuo <renyuzhuo@foxmail.com>
2017-11-30 14:02:46 +0800

    add no-op

---
+++ rviewer-no-op/src/main/java/cn/renyuzhuo/objectbox/viewer/ObjectViewBoxManager.java
@@ -0,0 +1,29 @@
+package cn.renyuzhuo.objectbox.viewer;
+
+import java.util.ArrayList;
+
+import io.objectbox.BoxStore;
+
+@SuppressWarnings("unused")
+public class ObjectViewBoxManager {
+
+    private static ObjectViewBoxManager mObjectViewBoxManager;
+
+    private ObjectViewBoxManager() {
+    }
+
+    public static ObjectViewBoxManager getInstance() {
+        if (mObjectViewBoxManager == null) {
+            synchronized (ObjectViewBoxManager.class) {
+                if (mObjectViewBoxManager == null) {
+                    mObjectViewBoxManager = new ObjectViewBoxManager();
+                }
+            }
+        }
+        return mObjectViewBoxManager;
+    }
+
+    public void init(BoxStore boxStore, ArrayList<Class> classes) {
+        mObjectViewBoxManager = null;
+    }
+}
