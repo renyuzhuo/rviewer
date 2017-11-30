923e9a3665fd6e1532e468cff8d5827dd09d43d9
RenYuZhuo <renyuzhuo@foxmail.com>
2017-11-30 15:33:31 +0800

    update name

--- app/src/main/java/cn/renyuzhuo/rviewerexample/App.java
+++ app/src/main/java/cn/renyuzhuo/rviewerexample/App.java
@@ -4,7 +4,6 @@ import android.app.Application;
 
 import java.util.ArrayList;
 
-import cn.renyuzhuo.objectbox.viewer.ObjectViewBoxManager;
 import io.objectbox.BoxStore;
 
 public class App extends Application {
@@ -20,7 +19,7 @@ public class App extends Application {
 
         ArrayList<Class> classes = new ArrayList<>();
         classes.add(Note.class);
-        ObjectViewBoxManager.getInstance().init(boxStore, classes);
+//        ObjectViewBoxManager.getInstance().init(boxStore, classes);
     }
 
     public static App get() {
